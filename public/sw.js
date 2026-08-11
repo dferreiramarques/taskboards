aad0daa2585e9bf6d2f7848922023b4d7b4a2e42
David Marques <dferreiramarques@gmail.com>
2026-08-11 12:01:33 +0100

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taskboards-v15';
+const CACHE_NAME = 'taskboards-v16';
 const ASSETS = ['/', './index.html', './style.css', './app.js', './manifest.json', './config.js'];
 
 self.addEventListener('install', e => {
